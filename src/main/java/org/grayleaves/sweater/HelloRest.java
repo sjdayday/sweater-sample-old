7a97e026ad662e419d5d0983f9197c31c2e29b28
Steve Doubleday <stevedoubleday@gmail.com>
2016-11-28 21:51:45 -0800

    begin sample app

---
+++ src/main/java/org/grayleaves/sweater/HelloRest.java
@@ -0,0 +1,14 @@
+package org.grayleaves.sweater;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/hello")
+public class HelloRest {
+
+	@GET
+	public String message() {
+		return "Hello, rest!"; 
+	}
+	
+}
---
+++ src/main/java/org/grayleaves/sweater/RestApp.java
@@ -0,0 +1,16 @@
+package org.grayleaves.sweater;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+	@ApplicationPath("/rest")
+	public class RestApp extends Application {
+	  public Set<Class<?>> getClasses() {
+	    return new HashSet<Class<?>>(Arrays.asList(HelloRest.class));
+	  }
+}
